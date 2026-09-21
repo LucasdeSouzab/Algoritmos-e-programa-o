ce7cf9dd82c5c47eb1c80040a26037ac9ca27492
Lucas de Souza <souzadelucas4@gmail.com>
2026-09-21 17:14:04 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador <= 20) {
+            System.out.println("Contagem:" + contador);
+            contador+=2;
+        }
+    }
+}
